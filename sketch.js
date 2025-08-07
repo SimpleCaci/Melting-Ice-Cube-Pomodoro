dedda43134f5f9f29c41ddb36949a169f65d0cf8
SimpleCaci <edikus@outlook.es>
2025-08-06 18:24:31 -0700

    basic structure figured out

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+let xPos = 20;
+let yPos = 20;
+let xVelo = 0;
+let yVelo = 0;
+let gravity = 1.1;
 function setup() {
   createCanvas(400, 400);
 }
@@ -5,4 +10,20 @@ function setup() {
 function draw() {
   background(220);
   ellipse(200, 200, 100, 100); // a circle
+  rect(xPos,yPos,40,40);
+  yVelo = yVelo + gravity;
+  yPos = yPos + yVelo;
+  if (yPos > (400-40)){
+    yPos = 400-40; // grounded it
+    yVelo = yVelo * -0.9 //made it bouncy
+    if (Math.abs(yVelo) < 2){
+        yVelo = 0;
+    }
+}
+}
+
+function doubleClicked(){
+    yPos = mouseY;
+    xPos = mouseX;
+    yVelo = 0;
 }
